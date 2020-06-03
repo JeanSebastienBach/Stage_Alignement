16340b4aaa34269f67fd9163df1523cc7c0c89ee
sentes <antoine.afflatet@gmail.com>
2020-06-03 10:23:57 +0200

    Calcul de probabilité d'apparition de nucléotide dans un tableau de séquence + modification de génération

--- generation.cpp
+++ generation.cpp
@@ -23,37 +23,179 @@ string nucleotideHasard(float matriceProba[]){
 	}
 }
 
-string generation(int t){
+void modificationTabProbaNucleotides(vector<string> tabSequence,float tabProbaNucleotides[4][4],unsigned int tabNbNucleotideConsiderer[4]){
 	string sequence = "";
-	int x=0;
+	char dernierNucleotide;
+	char actuelNucleotide;
+	cout<<"====================================================="<<endl;
+	cout<<"Liste des séquences considérer dans les probabilité : "<<endl;
+	cout<<endl;
+	for(int i=0;i<tabSequence.size();i++){
+		cout<<"S["<<i<<"] = "<<tabSequence[i]<<endl;
+		sequence = tabSequence[i];
+		for(int j=1;j<sequence.size();j++){
+			dernierNucleotide = sequence[j-1];
+			actuelNucleotide = sequence[j];
+			if(dernierNucleotide=='A'){
+				switch(actuelNucleotide){
+       		 		case 'A' :
+       		 			tabProbaNucleotides[0][0] = (((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][0])+1)/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][1] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][1])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][2] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][2])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][3] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][3])/(tabNbNucleotideConsiderer[0]+1));
+            			break;
 
-	if(t>1){
+        			case 'C' :
+        	    		tabProbaNucleotides[0][0] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][0])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][1] = (((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][1])+1)/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][2] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][2])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][3] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][3])/(tabNbNucleotideConsiderer[0]+1));
+        	    		break;
+			
+			        case 'G' :
+    		    	    tabProbaNucleotides[0][0] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][0])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][1] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][1])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][2] = (((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][2])+1)/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][3] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][3])/(tabNbNucleotideConsiderer[0]+1));
+    			        break;
+			
+	    	    	case 'U' :
+	    		        tabProbaNucleotides[0][0] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][0])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][1] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][1])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][2] = ((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][2])/(tabNbNucleotideConsiderer[0]+1));
+       		 			tabProbaNucleotides[0][3] = (((tabNbNucleotideConsiderer[0]*tabProbaNucleotides[0][3])+1)/(tabNbNucleotideConsiderer[0]+1));
+			            break;
+				}
+				tabNbNucleotideConsiderer[0]++;
+			}
+			else if(dernierNucleotide=='C'){
+				switch(actuelNucleotide){
+       		 		case 'A' :
+       		 			tabProbaNucleotides[1][0] = (((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][0])+1)/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][1] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][1])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][2] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][2])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][3] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][3])/(tabNbNucleotideConsiderer[1]+1));
+            			break;
+
+        			case 'C' :
+        	    		tabProbaNucleotides[1][0] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][0])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][1] = (((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][1])+1)/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][2] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][2])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][3] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][3])/(tabNbNucleotideConsiderer[1]+1));
+        	    		break;
+			
+			        case 'G' :
+    		    	    tabProbaNucleotides[1][0] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][0])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][1] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][1])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][2] = (((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][2])+1)/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][3] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][3])/(tabNbNucleotideConsiderer[1]+1));
+    			        break;
+			
+	    	    	case 'U' :
+	    		        tabProbaNucleotides[1][0] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][0])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][1] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][1])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][2] = ((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][2])/(tabNbNucleotideConsiderer[1]+1));
+       		 			tabProbaNucleotides[1][3] = (((tabNbNucleotideConsiderer[1]*tabProbaNucleotides[1][3])+1)/(tabNbNucleotideConsiderer[1]+1));
+			            break;
+				}
+				tabNbNucleotideConsiderer[1]++;
+			}
+			else if(dernierNucleotide=='G'){
+				switch(actuelNucleotide){
+       		 		case 'A' :
+       		 			tabProbaNucleotides[2][0] = (((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][0])+1)/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][1] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][1])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][2] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][2])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][3] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][3])/(tabNbNucleotideConsiderer[2]+1));
+            			break;
+
+        			case 'C' :
+        	    		tabProbaNucleotides[2][0] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][0])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][1] = (((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][1])+1)/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][2] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][2])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][3] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][3])/(tabNbNucleotideConsiderer[2]+1));
+        	    		break;
+			
+			        case 'G' :
+    		    	    tabProbaNucleotides[2][0] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][0])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][1] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][1])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][2] = (((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][2])+1)/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][3] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][3])/(tabNbNucleotideConsiderer[2]+1));
+    			        break;
+			
+	    	    	case 'U' :
+	    		        tabProbaNucleotides[2][0] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][0])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][1] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][1])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][2] = ((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][2])/(tabNbNucleotideConsiderer[2]+1));
+       		 			tabProbaNucleotides[2][3] = (((tabNbNucleotideConsiderer[2]*tabProbaNucleotides[2][3])+1)/(tabNbNucleotideConsiderer[2]+1));
+			            break;
+				}
+				tabNbNucleotideConsiderer[2]++;
+			}
+			else if(dernierNucleotide=='U'){
+				switch(actuelNucleotide){
+       		 		case 'A' :
+       		 			tabProbaNucleotides[3][0] = (((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][0])+1)/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][1] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][1])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][2] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][2])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][3] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][3])/(tabNbNucleotideConsiderer[3]+1));
+            			break;
+
+        			case 'C' :
+        	    		tabProbaNucleotides[3][0] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][0])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][1] = (((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][1])+1)/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][2] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][2])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][3] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][3])/(tabNbNucleotideConsiderer[3]+1));
+        	    		break;
+			
+			        case 'G' :
+    		    	    tabProbaNucleotides[3][0] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][0])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][1] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][1])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][2] = (((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][2])+1)/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][3] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][3])/(tabNbNucleotideConsiderer[3]+1));
+    			        break;
+			
+	    	    	case 'U' :
+	    		        tabProbaNucleotides[3][0] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][0])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][1] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][1])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][2] = ((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][2])/(tabNbNucleotideConsiderer[3]+1));
+       		 			tabProbaNucleotides[3][3] = (((tabNbNucleotideConsiderer[3]*tabProbaNucleotides[3][3])+1)/(tabNbNucleotideConsiderer[3]+1));
+			            break;
+				}
+				tabNbNucleotideConsiderer[3]++;
+			}
+		}
+	}
+	cout<<"====================================================="<<endl;
+} 
+
+string generation(int tailleSequenceAGenerer,float tabProbaNucleotides[4][4]){
+	string sequence = "";
+	int indice=0;
+
+	if(tailleSequenceAGenerer>1){
 		// Nucléotide de départ 
 		float teteSequence[4] = {0.250,0.250,0.250,0.250};
 		sequence += nucleotideHasard(teteSequence);
-		x++;
+		indice++;
 	}
 
-	while(t>x){
+	while(tailleSequenceAGenerer>indice){
 		char dernierNucleotide = sequence.back();
-		float sequenceA[4] = {0.300,0.205,0.285,0.210};
-		float sequenceC[4] = {0.322,0.298,0.078,0.302};
-		float sequenceG[4] = {0.248,0.246,0.298,0.208};
-		float sequenceU[4] = {0.177,0.239,0.292,0.292};
 
 		if(dernierNucleotide=='A'){
-			sequence += nucleotideHasard(sequenceA);
+			sequence += nucleotideHasard(tabProbaNucleotides[0]);
 		}
 		else if(dernierNucleotide=='C'){
-			sequence += nucleotideHasard(sequenceC);
+			sequence += nucleotideHasard(tabProbaNucleotides[1]);
 		}
 		else if(dernierNucleotide=='G'){
-			sequence += nucleotideHasard(sequenceG);
+			sequence += nucleotideHasard(tabProbaNucleotides[2]);
 		}
 		else if(dernierNucleotide=='U'){
-			sequence += nucleotideHasard(sequenceU);
+			sequence += nucleotideHasard(tabProbaNucleotides[3]);
 		}
-		x++;
+		indice++;
 	}
 
 	return sequence;
@@ -133,28 +275,62 @@ vector<string> generationMutation(string s,int nbSeq){
 
 int main(int argc, char** argv,char** env){
 	if(argc<4){
-		cout<<"La génération nécessite <mode> <taille_des_séquences_à_générer> <nombre_de_séquences_à_générer> "<<endl;
+		cout<<"La génération comporte deux mode :"<<endl;
+		cout<<"Mode 0 : génération "<<endl;nécessite <mode> <taille_des_séquences_à_générer> <nombre_de_séquences_à_générer> "<<endl;
 	}else {
 		srand(time(NULL));
-		string s ="";
+		string sequence ="";
 		int tailleSeq = stoi(argv[2]);
 		int nbSeq = stoi(argv[3]);
-		if (stoi(argv[1])==1){
-			while(s.size()!=tailleSeq){
+		int mode = stoi(argv[1]);
+		if (mode==1){
+			while(sequence.size()!=tailleSeq){
 				
 				cout<<"Veuillez saisir une séquence de taille "<<tailleSeq<<" : ";
-				cin>>s;
+				cin>>sequence;
 			}
-		}else{
-			
-			s = generation(tailleSeq);
-			//cout<<s<<endl;
+		}else {
+			vector<string> tabSequence;
+			tabSequence.push_back("ACGGUGACACUAACGCGAUGGCUAGCAGCUAGUCAUCGACCCAGUACGCAUG");
+			tabSequence.push_back("CAUGACCCGUGCAGCUAGCUACGAAAACUCGAUGAUCGUGCAUCAUGUCAGUCAGUG");
+			unsigned int tabNbNucleotideConsiderer[4];
+			float tabProbaNucleotides[4][4];
+			for (int k=0;k<4;k++){
+				tabProbaNucleotides[k][0]=0.0;
+				tabProbaNucleotides[k][1]=0.0;
+				tabProbaNucleotides[k][2]=0.0;
+				tabProbaNucleotides[k][3]=0.0;
+				tabNbNucleotideConsiderer[k]=0;
+			}
+			modificationTabProbaNucleotides(tabSequence,tabProbaNucleotides,tabNbNucleotideConsiderer);
+			cout<<"Liste des probabilité d'apparition de chaque nucléotide du nombre de nucléotides considérés : "<<endl;
+			cout<<endl;
+			for(int i=0;i<4;i++){
+				cout<<"Probabilité d'apparition aprés le nucléotide "<< ((i==0) ? "A" : ((i==1) ? "C" : ((i==2) ? "G" : "U"))) <<endl;
+				cout<<"A = "<<tabProbaNucleotides[i][0]<<endl;
+				cout<<"C = "<<tabProbaNucleotides[i][1]<<endl;
+				cout<<"G = "<<tabProbaNucleotides[i][2]<<endl;
+				cout<<"U = "<<tabProbaNucleotides[i][3]<<endl;
+			}
+			cout<<"Nucleotides considérés aprés un : "<<endl;
+			cout<<"A = "<<tabNbNucleotideConsiderer[0]<<endl;
+			cout<<"C = "<<tabNbNucleotideConsiderer[1]<<endl;
+			cout<<"G = "<<tabNbNucleotideConsiderer[2]<<endl;
+			cout<<"U = "<<tabNbNucleotideConsiderer[3]<<endl;
+			cout<<"====================================================="<<endl;
+			cout<<"Génération de la séquence de taille "<<tailleSeq<<" : "<<endl;
+			cout<<endl;
+			sequence = generation(tailleSeq,tabProbaNucleotides);
+			cout<<sequence<<endl;
+			cout<<"====================================================="<<endl;
 		}
-		vector<string> ensembleSeq = generationMutation(s,nbSeq);
-		for(int i=0;i<ensembleSeq.size();i++){
-			cout<<ensembleSeq[i]<<" ";
-		}
+		vector<string> ensembleSeq = generationMutation(sequence,nbSeq);
+		cout<<"Liste des mutation générer : "<<endl;
 		cout<<endl;
+		for(int i=0;i<ensembleSeq.size();i++){
+			cout<<"S["<<i<<"]="<<ensembleSeq[i]<<endl;
+		}
+		cout<<"====================================================="<<endl;
 	}
 	
 	return 0;
