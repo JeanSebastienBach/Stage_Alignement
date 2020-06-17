ef7202ca84dfa0e05a9d184a605cea54a83d1719
sentes <antoine.afflatet@gmail.com>
2020-06-17 16:49:13 +0200

    version corrigé d'arbre.sh

--- arbre.sh
+++ arbre.sh
@@ -3,10 +3,22 @@
 clear
 g++ alignementMultiple.cpp -Wall -o alignementMultiple
 g++ generation.cpp -Wall -o generation
-g++ comparaisonMast.cpp -Wall -o comparaisonMast
-g++ comparaisonZS.cpp -Wall -o comparaisonZS
-./generation $@ -o resultat.txt -f -om arbreMutation.dot
-./alignementMultiple -o arbre.dot -is resultat.txt
-dot -Tpng arbre.dot > arbre.png
-dot -Tpng arbreMutation.dot > arbreMutation.png
-./comparaisonZS -am arbre.dot -ag arbreMutation.dot
+nomDeFichier=$(date +%Y%m%d)
+./generation $@ resultat.txt -f -om arbreMutation.dot > $nomDeFichier
+
+cat $nomDeFichier
+erreurGeneration=$(grep -c 'generation()' $nomDeFichier)
+if (($erreurGeneration == 0))
+then
+	./alignementMultiple -o arbre.dot -is resultat.txt > $nomDeFichier
+	cat $nomDeFichier
+	erreurAlignement=$(grep -c 'alignement()' $nomDeFichier)
+	if (($erreurAlignement == 0))
+	then
+		dot -Tpng arbre.dot > arbre.png
+		dot -Tpng arbreMutation.dot > arbreMutation.png
+	fi
+fi
+rm $nomDeFichier
+
+
